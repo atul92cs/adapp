410d00d7305fa304fe70aff6d1fb537e3fb798e6
hthofficial <hthofficial2017@gmail.com>
2018-01-31 02:52:18 +0530

    first commit

---
+++ include/dboperations.php
@@ -0,0 +1,68 @@
+<?php
+class DbOperation
+{
+  private $con ;
+  function __construct()
+  {
+    require_once dirname(__FILE__).'/dbconnect.php';
+	$db=new DbConnect();
+	$this->con=$db->connect();
+  }
+  function registerUser($name,$password,$dob,$gender,$referal,$phone)
+	{
+		if(!$this->isUserExist($phone))
+		{
+		$pass=md5($password);
+		$stmt=$this->con->prepare("INSERT INTO user(name,password,dob,gender,referalcode,phone)VALUES(?,?,?,?,?,?)");
+		$stmt->bind_param("sssss",$name,$pass,$dob,$gender,$referal,$phone);
+		if($stmt->execute())
+			return USER_CREATED;
+		
+		return USER_CREATION_FAILED;
+		}
+		return USER_EXIST;
+	}
+	
+	function userLogin($phone,$password)
+	{
+		$pass=md5($password);
+		$stmt=$this->con->prepare("SELECT id FROM user WHERE phone=? AND password=?");
+		$stmt->bind_param("ss",$phone,$pass);
+		$stmt->execute();
+		$stmt->store_result();
+		return $stmt->num_rows>0;
+		
+	}
+  function userUpdate($id,$name,$phone,$password)
+	{
+		$pass=md5($password);
+		$stmt=$this->con->prepare("UPDATE users SET name=?phone=?,password=? WHERE id=?");
+		$stmt->bind_param("sssss",$name,$phone,$pass,$id);
+		if($stmt->execute())
+			return true;
+		return false;
+	}
+	function isUserExist($phone)
+	{
+		$stmt=$this->con->prepare("SELECT * FROM user WHERE phone=?");
+		$stmt->bind_param("s",$phone);
+		$stmt->execute();
+		$stmt->store_result();
+		return $stmt->num_rows>0;
+	}
+	function getUserByPhone($phone)
+	{
+		$stmt=$this->con->prepare("SELECT id,name,phone, FROM user WHERE phone=?");
+		$stmt->bind_param("s",$phone);
+		$stmt->execute();
+		$stmt->bind_result($id,$name,$phone);
+		$user=array();
+		$stmt->fetch();
+		$user['Id']=$id;
+		$user['Name']=$name;
+		$user['Phone']=$phone;
+		$user['Email']=$email;
+		return $user;
+		
+	}
+  }?>
